b54f9769c4f1fd41e79c6a63b3fe0aa185b361d4
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-31 10:47:18 -0300

    Criando components e exibindo imagem de fundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import Title from './component/title';
+import Counter from './component/counter';
+
+import background from './assets/background.jpg'
+
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>CountDown</h1>
+    <div className="App" style={{backgroundImage: `url(${background})`}}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2023"/>
+        <div className="countdown-container">
+          <Counter title="Dias" number={2}/>
+          <Counter title="Horas" number={2}/>
+          <Counter title="Minutos" number={2}/>
+          <Counter title="Segundo" number={2}/>
+        </div>
+      </div>
     </div>
   )
 };
