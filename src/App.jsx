32ea14072f695876114e72563f14a549f3e89abb
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-06-02 08:50:47 -0300

    Moldando os dados para ajustar o dias, horas, minutos e segundos. Já exibindo na tela e atualizando a cada segundo

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import useCountdown from './hooks/useCountdown';
 import './App.css';
 
 function App() {
-  const x = useCountdown("Jan 1, 2023 00:00:00")
+  const [day, hour, minute, second] = useCountdown("Jun 6, 2023 00:00:00")
   
   return (
 
@@ -16,10 +16,10 @@ function App() {
       <div className="container">
         <Title title="Contagem regressiva para 2023"/>
         <div className="countdown-container">
-          <Counter title="Dias" number={2}/>
-          <Counter title="Horas" number={2}/>
-          <Counter title="Minutos" number={2}/>
-          <Counter title="Segundo" number={2}/>
+          <Counter title="Dias" number={day}/>
+          <Counter title="Horas" number={hour}/>
+          <Counter title="Minutos" number={minute}/>
+          <Counter title="Segundo" number={second}/>
         </div>
       </div>
     </div>
